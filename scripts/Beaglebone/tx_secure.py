15f974f8280fb2a41965806537182580b2c40ad3
Ruth Berkow <ruth.berkow@mail.mcgill.ca>
2015-11-20 11:03:52 -0500

    Added timing but I'm not sure what this will tell us

--- scripts/Beaglebone/tx_secure.py
+++ scripts/Beaglebone/tx_secure.py
@@ -9,12 +9,11 @@ from can.protocols.secure.securemessage import SecureMessage
 from can.protocols.secure.arbitrationid import ArbitrationID
 from can.protocols.secure import Bus
 
-interface = 'can0'
+interface = 'vcan0'
 
 bus = Bus(channel=interface, claimed_addresses=[0, 1])
-print bus.local_node.address
+time.clock()
 arb = ArbitrationID(priority=5, destination_addresses = [1], source_address=bus.local_node.address)
-for i in range(49, 0, -1):
-	msg = SecureMessage(data=[i], arbitration_id=arb)
-	bus.send(msg)
-	time.sleep(1)
+msg = SecureMessage(data=[50], arbitration_id=arb)
+bus.send(msg)
+print time.clock()
