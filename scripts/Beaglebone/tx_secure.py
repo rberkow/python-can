ee71ab7e7bf060f8fdeeccd7131e8980812c71a7
Ruth Berkow <ruth.berkow@mail.mcgill.ca>
2015-12-07 17:32:09 -0500

    finished adding measurements

--- scripts/Beaglebone/tx_secure.py
+++ scripts/Beaglebone/tx_secure.py
@@ -11,17 +11,22 @@ from can.protocols.secure import Bus
 
 interface = 'vcan0'
 
-NO_OF_MSGS = 99
+NO_OF_MSGS = 100
+USEFUL_BITS = 40
 
 bus = Bus(channel=interface, claimed_addresses=[0, 1])
-arb = ArbitrationID(priority=5, destination_addresses = [1], source_address=bus.local_node.address)
+arb = ArbitrationID(priority=5, destination_addresses=[1], source_address=bus.local_node.address)
 sum = 0
-for i in range(NO_OF_MSGS, 0, -1):
+
+
+for i in range(NO_OF_MSGS - 1, 0, -1):
     start_time = time.time()
     msg = SecureMessage(data=[i], arbitration_id=arb)
     bus.send(msg)
     timer = time.time() - start_time
     sum += timer
 
+time_per_msg = sum / NO_OF_MSGS
 
-print "average sending time per msg: ", sum/NO_OF_MSGS
+print "latency: ", time_per_msg
+print "throughput: ", USEFUL_BITS / time_per_msg
--- scripts/Beaglebone/tx_std.py
+++ scripts/Beaglebone/tx_std.py
@@ -5,11 +5,23 @@ Script to run from transmitting Beaglebone to send standard CAN messages.
 import can
 import time
 from can.message import Message
+interface = 'vcan0'
+bustype = 'socketcan_ctypes'
+bus = can.interface.Bus(channel=interface, bustype=bustype)
 
-bus = can.interface.Bus()
+NO_OF_MSGS = 100
+USEFUL_BITS = 64
 
-for i in range(49, 0, -1):
-	msg = Message(data=[i], arbitration_id=0x14240000)
-	bus.send(msg)
-	time.sleep(1)
+sum = 0
 
+for i in range(NO_OF_MSGS - 1, 0, -1):
+    start_time = time.time()
+    msg = Message(data=[i], arbitration_id=0x14240000)
+    bus.send(msg)
+    timer = time.time() - start_time
+    sum += timer
+
+time_per_msg = sum / NO_OF_MSGS
+
+print "latency: ", time_per_msg
+print "throughput: ", USEFUL_BITS / time_per_msg
